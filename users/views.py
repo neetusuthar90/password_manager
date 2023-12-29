3448782d7dc82f080ee4501b6b9ac9ae0390d1b0
neetusuthar90 <neetusuthar90@gmail.com>
2023-12-29 23:52:34 +0530

    Features I am adding and expanding

--- users/views.py
+++ users/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import RegistrationForm
 
-def index(request):
-    return HttpResponse("Hello world!!")
+def home(request):
+    return render(request, 'users/home.html')
+
+#create registration form
+def register(request):
+    submitted = False
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/register?submitted=True')
+    else:
+        form = RegistrationForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'users/add_user.html', {'form': form, 'submitted':submitted})
